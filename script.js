d8ff86536a8ef4865d4c1a34f6020d0d2673b35a
EdgarOsepian <85243883+EdgarOsepian@users.noreply.github.com>
2024-07-26 16:10:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Name is : Edgar");
+console.log("Surname is : Osepian");
+console.log("Age : 100");
+console.warn("Email : Test@gmail.com");
+console.warn("Password : ******");
+console.log("Phone : 555-555-555");
